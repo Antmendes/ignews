28db0cd73255698fe0e52786578532c2541d6cbc
Antmendes <antoniomendesjunior02@gmail.com>
2023-06-28 21:15:28 -0300

    configurando requisiçao a api do stripe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 import { Header } from "./components/Header/page";
 import { SubscribeButton } from "./components/SubscribeButton/page";
 import styles from './home.module.scss'
+import { stripe } from "./services/stripe";
 
-export default function Home() {
+
+export default async function Home() {
+  const stripe = require('stripe') ('sk_test_51NO61TGl9kA9zGAJkKmHT02iyawXlmoNzXMmhiJIabxmivd1ty6VMShdfstfFApGDLAjmrpqD0ldovSTRFoChHo100LuXnA9lM')
+
+  const price = await stripe.prices.retrieve('price_1NO6FgGl9kA9zGAJPLMmJ15W' )
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  
   return (
     <>
     <Header />
@@ -13,9 +28,9 @@ export default function Home() {
         <h1>News about the <span>React</span> world.</h1>
         <p>
           Get acces to all the publications <br />
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId}/>
       </section>
 
       <img src="/images/Mulher.png" alt="Girl coding" />
