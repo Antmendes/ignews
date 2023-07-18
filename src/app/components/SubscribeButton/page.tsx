d80a52971b2351fa3fbc05c436b308218d33df26
Antmendes <antoniomendesjunior02@gmail.com>
2023-07-18 18:15:25 -0300

    criando sessao de checkout com stripe

--- src/app/components/SubscribeButton/page.tsx
+++ src/app/components/SubscribeButton/page.tsx
@@ -1,14 +1,36 @@
+'use client'
+
+import { signIn, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
+import React from 'react'
+import { Checkout_sessions } from '@/app/api/checkout_sessions'
+
+
+
+
+
+
 
 interface SubscribeButtonProps {
     priceId: number
 }
 
-export function SubscribeButton({ priceId}: SubscribeButtonProps) {
-    return (
+
+
+export async function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  
+  
+   
+
+   return (
         <button
           type="button"
           className={styles.subscribebutton}
+          onClick={(() => {
+            Checkout_sessions({
+              lineItems:[{price:'price_1NO6FgGl9kA9zGAJPLMmJ15W', quantity: 1}]
+            })
+          })}
         >
           Subscribe now
         </button>
